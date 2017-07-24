d43df0054358db88295cf41b27066397375ae662
xiaocheng <707996290@qq.com>
2017-07-24 22:05:17 +0800

    canvas mouse 事件
    canvas mouse 事件

--- writeWord/js/index.js
+++ writeWord/js/index.js
@@ -2,15 +2,42 @@ window.onload = function() {
     word.init();
 }
 var word = {
+    /**
+     * @parmas {Number} canvasWidth canvas 画布的宽高
+     * @parmas {String} canvasId canvas 画布的 id
+     * @parmas context 画布的笔触 （自己的理解）
+     * @parmas {Boolean} isMovesDown 鼠标是否按下 false
+     */
     canvasWidth: 800,
     canvasId: 'canvas',
     context: '',
+    isMouseDown: false,
     // 初始化
     init: function() {
         var canvas = document.getElementById(this.canvasId);
         canvas.width = canvas.height = this.canvasWidth;
         this.context = canvas.getContext('2d');
         this.drawTable();
+    
+        canvas.onmousedown = function(event) {
+            event.preventDefault();
+            this.isMouseDown = true;
+            console.log("mouse down")
+        }
+        canvas.onmouseup = function(event) {
+            event.preventDefault();
+            this.isMouseDown = false;
+        }
+        canvas.onmouseout = function(event) {
+            event.preventDefault();
+            this.isMouseDown = false;
+        }
+        canvas.onmousemove = function(event) {
+            event.preventDefault();
+            if(this.isMouseDown) {
+                // ...
+            }
+        }
     },
     // 画米字格
     drawTable: function() {
